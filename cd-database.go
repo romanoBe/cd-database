163273999e8af4f154fce914edd1c964b875453a
romanoBe <72611433+romanoBe@users.noreply.github.com>
2022-02-01 20:14:57 +0100

    Initial version of adding albums

--- cd-database.go
+++ cd-database.go
@@ -2,25 +2,27 @@ package main
 
 import (
 	"bufio"
+	"fmt"
 	"os"
 )
 
 const programTitle = "CD Database"
 
 func main() {
+	var albums string
 	println(programTitle)
 
 	for {
 		println("Type in command: q - quit, l - list CDs, a - add CD, d - delete CD")
 		cmd := readCmd()
 		switch cmd {
-		case 'q':
+		case 'q', 'Q':
 			return
-		case 'l':
-			listCDs()
-		case 'a':
-			addCD()
-		case 'd':
+		case 'l', 'L':
+			listCDs(albums)
+		case 'a', 'A':
+			albums = addCD()
+		case 'd', 'D':
 			deleteCD()
 		}
 	}
@@ -32,14 +34,23 @@ func readCmd() rune {
 	return cmd
 }
 
-func listCDs() {
+func listCDs(albums string) {
 	println("Displaying list of CDs")
+	println(albums)
 }
 
-func addCD() {
+func addCD() string {
 	println("Adding new CD")
+	return readString("Input album title: ")
 }
 
 func deleteCD() {
 	println("Delete CD")
+}
+
+func readString(title string) string {
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Print(title)
+	input, _ := reader.ReadString('\n')
+	return input
 }
